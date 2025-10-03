30d4c4a6c688c85978b79eea555b0786bd1856d4
Hizan Ali <148907510+hizanali7@users.noreply.github.com>
2025-10-03 11:44:21 +0300

    Time: 3 ms (89.93%) | Memory: 61 MB (37.72%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(ali) {
+        let val = {I:1,V:5,X:10,L:50,C:100,D:500,M:1000};
+    let num = 0;
+    
+    for (let i=0;i<ali.length;i++){
+        let a = val[ali[i]], next = val[ali[i+1]];
+        num += (next && a < next) ? -a : a;
+    }
+    return num;
+};
